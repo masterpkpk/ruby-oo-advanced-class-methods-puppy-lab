4c864400b3f155cc3746fbf69e945c9edc45f3e8
william <masterpkpk@gmail.com>
2020-11-20 22:20:07 -0500

    commit tree f04c2f776f992acd181681c2c50899f64fc508de

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        self.name = (name)
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each {|name| puts name.name}
+    end
+
+    def save
+        @@all << self
+    end
+end
